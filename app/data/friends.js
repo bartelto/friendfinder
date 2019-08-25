ebbcaa0697bf51cfe62f0515b7b62bf8840936b6
Miles Standoffish <toddnan@gmail.com>
2019-08-25 15:30:43 -0500

    First commit

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+friends = [
+    {
+        name: "Cookie Monster",
+        photo: "https://pbs.twimg.com/profile_images/1092451626781163523/0YzJMi-8_400x400.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
