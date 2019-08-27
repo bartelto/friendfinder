2ec91ca4f743cf1a581ff8d52887f795810ec59c
Miles Standoffish <toddnan@gmail.com>
2019-08-26 22:25:43 -0500

    response modal working

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,18 +2,27 @@ friends = [
     {
         name: "Cookie Monster",
         photo: "https://pbs.twimg.com/profile_images/1092451626781163523/0YzJMi-8_400x400.jpg",
-        scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
+        scores: [5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        name: "Big Bird",
+        photo: "https://pbs.twimg.com/profile_images/1092452187777703936/acEpmmyi_400x400.jpg",
+        scores: [5,1,2,5,4,1,5,1,4,4]
+    },
+    {
+        name: "Oscar the Grouch",
+        photo: "https://pbs.twimg.com/profile_images/1092452335446568961/nhIaXRoF_400x400.jpg",
+        scores: [5,1,5,4,1,4,4,5,1,2,]
+    },
+    {
+        name: "Grover",
+        photo: "https://pbs.twimg.com/profile_images/1092454427049578499/SMRmdbPX_400x400.jpg",
+        scores: [5,1,2,5,4,1,1,4,4,5]
+    },
+    {
+        name: "Rowlf the Dog",
+        photo: "https://vignette.wikia.nocookie.net/puppet/images/5/53/Rowlf_the_Dog_1.jpg/revision/latest?cb=20111004013334",
+        scores: [5,1,5,5,1,1,2,5,2,2]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,31 @@ module.exports = function(app) {
         res.json(friends);
     });
 
+    // client posts survey answers and receives their closest match from the 'friends' array
     app.post("/api/friends", function(req, res){
+        console.log("POST request at /api/friends");
+        let user = req.body;
+        console.log(user);
 
+        let bestFriend = {};
+        let bestCompatibility = Infinity;
+        friends.forEach(function(friend) {
+            let compatibility = 0;
+            for (let i=0; i<friend.scores.length; i++) {
+                user.scores[i] = parseInt(user.scores[i]);
+                compatibility += Math.abs(user.scores[i] - friend.scores[i]);
+            }
+            console.log("compatibility: " + compatibility);
+            if (compatibility<bestCompatibility) {
+                bestCompatibility = compatibility;
+                bestFriend = friend;
+            }
+        });
+        res.json({
+            name: bestFriend.name,
+            photo: bestFriend.photo
+        });
+
+        friends.push(user)
     });
 }
