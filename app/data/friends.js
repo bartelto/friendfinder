e11545111dd3c8df3b4328dd1d09a9e15cf38579
Miles Standoffish <toddnan@gmail.com>
2019-08-28 12:17:44 -0500

    Muppet questions & styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,52 @@ friends = [
     {
         name: "Cookie Monster",
         photo: "https://pbs.twimg.com/profile_images/1092451626781163523/0YzJMi-8_400x400.jpg",
-        scores: [5,1,4,4,5,1,2,5,4,1]
+        scores: [5,3,3,1,5,4,2,3,2,3]
     },
     {
         name: "Big Bird",
         photo: "https://pbs.twimg.com/profile_images/1092452187777703936/acEpmmyi_400x400.jpg",
-        scores: [5,1,2,5,4,1,5,1,4,4]
+        scores: [4,5,1,5,4,1,4,3,4,4]
     },
     {
         name: "Oscar the Grouch",
         photo: "https://pbs.twimg.com/profile_images/1092452335446568961/nhIaXRoF_400x400.jpg",
-        scores: [5,1,5,4,1,4,4,5,1,2,]
+        scores: [1,1,5,1,1,4,1,1,1,2]
     },
     {
         name: "Grover",
         photo: "https://pbs.twimg.com/profile_images/1092454427049578499/SMRmdbPX_400x400.jpg",
-        scores: [5,1,2,5,4,1,1,4,4,5]
+        scores: [3,5,2,5,3,2,4,3,4,4]
+    },
+    {
+        name: "Ernie",
+        photo: "https://pbs.twimg.com/profile_images/1092454153585082368/QNM6C86g_400x400.jpg",
+        scores: [4,4,2,4,5,1,4,4,4,4]
+    },
+    {
+        name: "Bert",
+        photo: "https://pbs.twimg.com/profile_images/1092453930401968129/xn0CkJT6_400x400.jpg",
+        scores: [2,2,4,2,1,5,3,2,2,1]
     },
     {
         name: "Rowlf the Dog",
         photo: "https://vignette.wikia.nocookie.net/puppet/images/5/53/Rowlf_the_Dog_1.jpg/revision/latest?cb=20111004013334",
-        scores: [5,1,5,5,1,1,2,5,2,2]
+        scores: [2,1,3,3,1,4,5,4,3,2]
+    },
+    {
+        name: "Fozzie Bear",
+        photo: "https://vignette.wikia.nocookie.net/muppet/images/b/b5/Fozzie2.jpg/revision/latest?cb=20120410231906",
+        scores: [4,3,2,4,2,2,3,5,3,3]
+    },
+    {
+        name: "Miss Piggy",
+        photo: "https://pbs.twimg.com/profile_images/77813395/Piggy1_400x400.jpg",
+        scores: [5,1,3,3,4,3,3,2,5,2]
+    },
+    {
+        name: "Kermit the Frog",
+        photo: "https://pbs.twimg.com/profile_images/1080855598298611713/lTS-u1Iu.jpg",
+        scores: [3,3,2,4,1,2,4,3,4,5]
     }
 ];
 
